f37a3a78e3d8a7909934a50571d9a63090e2c61a
Jieun Ko <0930ko@naver.com>
2022-04-13 11:33:18 +0900

    relocate/제트카라커스텀

---
+++ Project JetKara Custom/My project/Assets/jetkara/Scripts/Fire.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+
+public class Fire : MonoBehaviour
+{
+	float _timefire;
+	float _timelight;
+
+	void OnEnable () 
+	{
+		_timefire = Time.time + 0.3f;
+		_timelight = Time.time + 0.4f;
+	}
+	
+	void FixedUpdate () 
+	{
+		if (gameObject.name == "rocket1")
+		{
+			if (_timefire < Time.time)
+				gameObject.SetActive(false);
+		}
+		else if (gameObject.name == "rocket2")
+        {
+			if (_timelight < Time.time)
+				gameObject.SetActive(false);
+		}
+
+	}
+}
---
+++ Project JetKara Custom/My project/Assets/jetkara/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject[] objects;
+
+	public TextMesh scoreLabel;
+	public static int score;
+	public int Score
+	{
+		set
+		{
+			score = value;
+
+			scoreLabel.text = Score.ToString();
+		}
+		get
+		{
+			return score;
+		}
+	}
+
+	void Start () 
+	{
+		score = 0;
+
+		InvokeRepeating("CreateObjects", 1,2);
+
+		int objectNum = Random.Range(0, objects.Length);
+		Instantiate(objects[objectNum], new Vector3(4.5f, Random.Range(-2f, 2.1f), 0), Quaternion.identity);
+	}
+
+	void CreateObjects()
+	{
+		int objectNum = Random.Range(0, objects.Length);
+	
+		Instantiate(objects[objectNum], new Vector3(6.3f, Random.Range(-2f, 2.1f), 0), Quaternion.identity);
+		
+
+	}
+}
+
+
